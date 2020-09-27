6c8e89067f2fe80b5ea86a7611049c4bd480ba54
flintj451 <flintj451@gmail.com>
2020-09-28 03:40:08 +0700

    DaoFactory, DaoUtil and ResultSetParser were added

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,4 +1,77 @@
 package dao;
 
+
+import utils.Constants;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DaoFactory {
+
+    private static Logger log = Logger.getLogger(DaoFactory.class.getName());
+
+    private String url;
+    private String driver;
+    private String username;
+    private String password;
+
+    //  ?????
+    public DaoFactory(String programPath) throws IOException, ClassNotFoundException {
+        setProperties(programPath);
+        setDriver();
+    }
+
+    private void setDriver() throws ClassNotFoundException {
+        try {
+            if (driver != null) {
+                Class.forName(driver);
+            }
+        } catch (ClassNotFoundException e) {
+            log.log(Level.SEVERE, "Database Driver not found ", e);
+            throw e;
+        }
+    }
+
+    private void setProperties(String filePath) throws IOException {
+
+        Properties prop = new Properties();
+        try (FileInputStream fis = new FileInputStream(filePath + "application.properties")
+        ) {
+            if (fis != null) {
+                prop.load(fis);
+            } else {
+                throw new FileNotFoundException("Database properties file not found in a directory" + filePath);
+            }
+        } catch (FileNotFoundException e) {
+            log.log(Level.SEVERE, "Database properties file not found in a directory" + filePath, e);
+            throw e;
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "IOException during setProperties ", e);
+            throw e;
+        }
+        url = prop.getProperty(Constants.PROPERTY_URL);
+        driver = prop.getProperty(Constants.PROPERTY_DRIVER);
+        username = prop.getProperty(Constants.PROPERTY_USERNAME);
+        password = prop.getProperty(Constants.PROPERTY_PASSWORD);
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            log.log(Level.SEVERE, "Cannot getConnection ", e);
+            throw e;
+        }
+        return connection;
+    }
+
+
 }
--- src/main/java/dao/ResultSetParser.java
+++ src/main/java/dao/ResultSetParser.java
@@ -1,4 +1,83 @@
 package dao;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ResultSetParser {
+
+    private static Logger log = Logger.getLogger(ResultSetParser.class.getName());
+
+    static JSONArray parseCustomers(ResultSet resultSet) throws SQLException {
+        JSONArray customersList = new JSONArray();
+        while (resultSet.next()) {
+            try {
+                JSONObject customerName = new JSONObject();
+                customerName.put("lastName", resultSet.getString("lastName"));
+                customerName.put("firstName", resultSet.getString("firstName"));
+                customersList.add(customerName);
+            } catch (SQLException e) {
+                log.log(Level.WARNING, "sql exception occurred during customers resultSet parsing", e);
+                throw e;
+            }
+        }
+        log.log(Level.FINEST, "Customers resultSet has been parsed ");
+        return customersList;
+    }
+
+    static JSONArray parseCustomersInfo(ResultSet resultSet) throws SQLException {
+        JSONArray customersInfoList = new JSONArray();
+
+        while (resultSet.next()) {
+            try {
+                JSONObject customerInfo = new JSONObject();
+                customerInfo.put("id", resultSet.getLong("id"));
+                customerInfo.put("firstName", resultSet.getString("firstName"));
+                customerInfo.put("lastName", resultSet.getString("lastName"));
+                customersInfoList.add(customerInfo);
+            } catch (SQLException e) {
+                log.log(Level.WARNING, "sql exception occurred during customersInfo resultSet parsing", e);
+                throw e;
+            }
+        }
+        log.log(Level.FINEST, "CustomersInfo resultSet has been parsed ");
+        return customersInfoList;
+    }
+
+    static JSONArray parseCustomersPurchases(ResultSet resultSet) throws SQLException {
+        JSONArray purchasesList = new JSONArray();
+        while (resultSet.next()) {
+            try {
+                JSONObject purchase = new JSONObject();
+                purchase.put("name", resultSet.getString("productName"));
+                purchase.put("expenses", resultSet.getLong("sum"));
+                purchasesList.add(purchase);
+            } catch (SQLException e) {
+                log.log(Level.WARNING, "sql exception occurred during CustomersPurchases resultSet parsing", e);
+                throw e;
+            }
+        }
+        log.log(Level.FINEST, "CustomersPurchases resultSet has been parsed ");
+        return purchasesList;
+    }
+
+    static long parseTotalExpenses(ResultSet resultSet) throws SQLException {
+        long totalExpenses;
+        try {
+            if (resultSet.next()) {
+                totalExpenses = resultSet.getLong("sum");
+            } else {
+                totalExpenses = 0;
+            }
+        } catch (SQLException e) {
+            log.log(Level.WARNING, "sql exception occurred during CustomersPurchases resultSet parsing", e);
+            throw e;
+        }
+        log.log(Level.FINEST, "TotalExpenses resultSet has been parsed ");
+        return totalExpenses;
+    }
 }
