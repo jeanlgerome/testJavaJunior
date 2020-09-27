4130ba84f0e429e486f499203c028949aa26ed4b
flintj451 <flintj451@gmail.com>
2020-09-28 03:37:46 +0700

    InputReader and OutputWriter were added

--- src/main/java/reading/InputReaderImpl.java
+++ src/main/java/reading/InputReaderImpl.java
@@ -1,4 +1,48 @@
 package reading;
 
-public class InputReaderImpl {
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class InputReaderImpl implements InputReader {
+
+    private static Logger log = Logger.getLogger(InputReader.class.getName());
+    private JSONParser parser = new JSONParser();
+    private String filePath;
+
+    public InputReaderImpl(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * @throws IOException    If something fails during reading json file.
+     * @throws ParseException If something fails during parsing json file.
+     */
+    @Override
+    public JSONObject readInput() throws IOException, ParseException {
+        JSONObject inputJson;
+        try (FileInputStream fis = new FileInputStream(filePath);
+             BufferedReader bufferedReader =
+                     new BufferedReader(new InputStreamReader(fis, "windows-1251"))
+        ) {
+            Object obj = parser.parse(bufferedReader);
+            inputJson = (JSONObject) obj;
+            log.log(Level.FINEST, " input.json has been successfully read ");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "IOException in readInput", e);
+            throw e;
+        } catch (ParseException e) {
+            log.log(Level.SEVERE, " Exception during input.json parsing", e);
+            throw e;
+        }
+        return inputJson;
+    }
+
 }
--- src/main/java/writing/OutputWriterImpl.java
+++ src/main/java/writing/OutputWriterImpl.java
@@ -1,4 +1,31 @@
 package writing;
 
-public class OutputWriterImpl {
+import org.json.simple.JSONObject;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OutputWriterImpl implements OutputWriter {
+
+
+    private static Logger log = Logger.getLogger(OutputWriterImpl.class.getName());
+    private String filePath;
+
+    public OutputWriterImpl(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void writeOutputJson(JSONObject outputJson) {
+        try (OutputStreamWriter osw =
+                     new OutputStreamWriter(new FileOutputStream(filePath), "utf-8")) {
+            osw.write(outputJson.toJSONString());
+            log.log(Level.FINEST, "file has been written successfully");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "exception during outputJson writing", e);
+        }
+    }
 }
